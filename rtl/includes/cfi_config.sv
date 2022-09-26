89405e9540d4f6899efee19b63dcc6a247a25069
Alex <duuu.ich@gmail.com>
2022-09-26 15:12:43 +0200

    add capacity constant

--- rtl/includes/cfi_config.sv
+++ rtl/includes/cfi_config.sv
@@ -1,4 +1,10 @@
-// BACCTODO verify that this does not interfere with parameters
-`ifndef CFI_INSTR_WIDTH_DEF
+// File: cfi_config.sv
+// Constants for the Crypto CFI extension 
+
+`ifndef CFI_CONFIG_SV
+`define CFI_CONFIG_SV
+
 `define CFI_INSTR_WIDTH_DEF 40
+`define CFI_CAPACITY_DEF 160
+
 `endif
