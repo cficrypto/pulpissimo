b52cc31b96241d7cd98e53036d5b1217c323dd06
Alex <duuu.ich@gmail.com>
2022-11-04 18:29:19 +0100

    replace link by file

--- boot_code/include/cfi.h
+++ boot_code/include/cfi.h
@@ -1,1 +1,10 @@
-/home/alex/Documents/Uni/bacc/pulpissimo_benchmarks/common/cfi.h
+#ifndef CFI_H
+#define CFI_H
+
+#define CFI_CFG_ON 0x1
+#define CFI_CFG_OFF 0x0
+#define CFI_CFG_LOAD_STATE 0x2
+#define CFI_CFG_BASE 0x7d0
+#define CFI_TAG_BASE (CFI_CFG_BASE + 1)
+
+#endif // !CFI_H
