76e3e60413ff30ece26826ebb9abeb2f2f599e1b
Alex <duuu.ich@gmail.com>
2022-11-24 15:14:10 +0100

    add macro for bus expansion

--- rtl/includes/cfi_config.sv
+++ rtl/includes/cfi_config.sv
@@ -7,4 +7,7 @@
 `define CFI_INSTR_WIDTH_DEF 40
 `define CFI_CAPACITY_DEF 160
 
+// this value is used to fill the additional bits when extending from the 32 to CFI bus
+`define CFI_FILL_DEF 8'b0
+
 `endif
